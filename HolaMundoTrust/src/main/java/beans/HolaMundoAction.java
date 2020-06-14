33f7c4d9bd373ab08def84e4cdbf1226806080d8
franmauriz <franmaurizpereira@gmail.com>
2020-06-14 17:33:10 +0200

    Empezamos con el repositorio de los frameworks spring,hibernate y struts

---
+++ HolaMundoTrust/src/main/java/beans/HolaMundoAction.java
@@ -0,0 +1,27 @@
+package beans;
+
+import org.apache.logging.log4j.*;
+
+public class HolaMundoAction {
+    
+    Logger log = LogManager.getLogger(HolaMundoAction.class);
+    
+    private String saludosAtr;
+    
+    public String execute(){
+        log.info("ejecutando metodo execute desde struts 2");
+        this.saludosAtr="Saludos desde Struts 2";
+        return "exito";
+    }
+
+    public String getSaludosAtr() {
+        return saludosAtr;
+    }
+
+    public void setSaludosAtr(String saludosAtr) {
+        this.saludosAtr = saludosAtr;
+    }
+    
+    
+    
+}
